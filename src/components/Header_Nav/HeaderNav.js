6872a348ade9b0ad959e2ed35474189e454b9921
BF <burlferns@gamil.com>
2020-09-26 07:00:05 -0700

    Added Roboto fonts and updated index.css and deleted App.css

---
+++ src/components/Header_Nav/HeaderNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export default function HeaderNav() {
+  return (
+    <div>
+      <h1>Ride for Life</h1>
+    </div>
+  )
+}
