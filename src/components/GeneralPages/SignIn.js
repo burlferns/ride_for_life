7f1414bbf69e5bd3b09a4d29e789a68271e387af
BF <burlferns@gamil.com>
2020-10-02 15:19:18 -0700

    did styling for desktop but vh units in calc do not seem to work in SignIn.js

--- src/components/GeneralPages/SignIn.js
+++ src/components/GeneralPages/SignIn.js
@@ -13,34 +13,47 @@ const MarginDiv = styled.div`
 
 
 const DivContainer = styled.div`
-  display:flex;
-  flex-direction:columns;
+  width:95rem;
+  height:60rem;
   background:white;
   box-shadow:  4px 4px 3px 0px #f2f2f2;
-  margin:5rem 5rem;
   border:1px solid #e6e6e6;
+  box-sizing: content-box;
+
+  display:grid;
+  grid-template-columns: auto 34rem;
+  grid-template-rows: [first] 100% [end];
+  grid-template-areas: "picture content";
+  
+  // margin:0 auto;
+  margin: calc((100vh - 44px - 40px - 411px) / 2) auto;
 `; 
 
 const PictureDiv = styled.div`
-  width: 600px;
-  height:600px;
+  width: 60rem;
+  height: 60rem;
   background-image: url(${img1});
   background-size: cover;
   background-position: bottom center;
+
+  grid-area: picture;
 `;
 
 
 const ContentsDiv = styled.div`
-  margin:0 4rem;
+  padding: 0 4rem;
+  margin: auto 0;
   width:fit-content;
   box-sizing: content-box;
-  border:1px solid black;
+
+  grid-area: content;
+  // border:1px solid black;
 `;
 
 
 const StylH1 = styled.h1`
-  font-size: 4rem;
-  margin-bottom: 2rem;
+  font-size: 2.5rem;
+  margin-bottom: 2.5rem;
 `;
 
 const StylSignInForm = styled(SignInForm)`
@@ -59,17 +72,10 @@ export default function SignIn() {
       <DivContainer>
         <PictureDiv></PictureDiv>
         <ContentsDiv>
-          <StylH1>Sign-In</StylH1>   
+          <StylH1>Login to Ride for Life</StylH1>   
           <StylSignInForm/>
         </ContentsDiv>
       </DivContainer>
-
-      
-
-
-      
-
-
 
     </>
     );
