c4beeaed97a9627a75b294df53920fc3fcce5888
BF <burlferns@gamil.com>
2020-10-10 19:05:05 -0700

    added update profile link to driver profile page

--- src/components/DriverPages/DriverProfile.js
+++ src/components/DriverPages/DriverProfile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import {useSelector} from 'react-redux';
+import {Link} from "react-router-dom";
 
 const StylH1 = styled.h1`
   margin:5rem 5rem;
@@ -13,16 +14,17 @@ const selectorFunc = state=>state.userData;
 
 export default function(props) {
   const className = props.className;
-  const userData = useSelector(selectorFunc);
+  const driverData = useSelector(selectorFunc);
 
   return (
     <>
       <StylH1>This is the Driver Profile page</StylH1>
-      <p>driver name : {userData.drivers_name}</p>
-      <p>driver plot : {userData.drivers_plot}</p>
-      <p>driver phone number : {userData.drivers_phone_number}</p>
-      <p>driver email : {userData.drivers_email}</p>
-      <p>driver price : {userData.drivers_price}</p>
+      <p>driver name : {driverData.drivers_name}</p>
+      <p>driver plot : {driverData.drivers_plot}</p>
+      <p>driver phone number : {driverData.drivers_phone_number}</p>
+      <p>driver email : {driverData.drivers_email}</p>
+      <p>driver price : {driverData.drivers_price}</p>
+      <Link to='/driver/updateprofile'>Update Profile</Link>
     </>
   );
 }
