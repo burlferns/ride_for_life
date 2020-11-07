f9613348340b193883e3254f308dd2097b528f25
BF <burlferns@gamil.com>
2020-11-06 18:14:47 -0800

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -5,19 +5,36 @@ import {createStore, applyMiddleware} from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css';
 import App from './App.js';
 import rootReducer from './reducers/rootReducer.js';
 
-const store = createStore(rootReducer, applyMiddleware(thunk,logger));
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+  whitelist: ['userData']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const store = createStore(persistedReducer, applyMiddleware(thunk,logger));
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
